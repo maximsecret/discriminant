227476c2c5bd35b8aff36794fba8672ebb6792ec
maximsecret <maximsecret12@gmail.com>
2015-07-13 13:30:59 +0600

    Based discriminant

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <locale>
 #include <cstring>
+#include <cmath>
 
 struct st_language{
     wchar_t input[10];
@@ -8,10 +9,8 @@ struct st_language{
 };
 
 void discr(st_language language);
-inline void inp_num_abc(int &a, int &b, int &c , wchar_t *t_in);
+inline void inp_num(int &a, wchar_t *t_in,wchar_t* t_a);
 void set_text(st_language &language);
-
-
 
 int main()
 {
@@ -22,32 +21,37 @@ int main()
     return 0;
 }
 
+
+
 void discr(st_language language){
     int a,b,c;
-    float d;
+    float d,x1,x2;
     //wchar_t t_in[10] = L"Введите";
 
 
     wprintf(L"Вычислитель дискриминанта\n");
-    inp_num_abc(a,b,c,language.input);
+    wchar_t i[1];
 
-    d = b;
+    //Вводим переменные
+    inp_num(a,language.input,wcscpy(i,L"a"));
+    inp_num(b,language.input,wcscpy(i,L"b"));
+    inp_num(c,language.input,wcscpy(i,L"c"));
+    //Вычисляем дискриминант
+    d = b*b - 4 * a * c  ;
+    x1 = (-b + sqrt(d))/(2*a);
+    x2 = (-b - sqrt(d))/(2*a);
 
-    wprintf(L"d = %i",d);
+    wprintf(L"d = %F\n",d);
+    wprintf(L"x1 = %F\n",x1);
+    wprintf(L"x2 = %F\n",x2);
 
     //wprintf(L"a = %i\nb = %i\nc = %i\n",a,b,c);
 
 }
 
-inline void inp_num_abc(int &a,int &b, int &c ,wchar_t* t_in){
-    wprintf(L"%ls %s: ",t_in,"a");
+inline void inp_num(int &a ,wchar_t* t_in,wchar_t* t_a){
+    wprintf(L"%ls %s: ",t_in,t_a);
     std::cin >> a;
-
-    wprintf(L"%ls %s: ",t_in,"b");
-    std::cin >> b;
-
-    wprintf(L"%ls %s: ",t_in,"c");
-    std::cin >> c;
 }
 
 void set_text(st_language &language){
