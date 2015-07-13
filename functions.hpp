f3d95487ae3889b216571ffb6c0cedf5c6d8f2ff
maximsecret <maximsecret12@gmail.com>
2015-07-13 23:46:19 +0600

    worked version

---
+++ functions.hpp
@@ -0,0 +1,5 @@
+void discr(st_language language);
+inline void inp_num(int &a, st_language language,wchar_t* t_a);
+inline void s_clear(int x);
+inline void d_intro(st_language language);
+void set_text(st_language &language);
